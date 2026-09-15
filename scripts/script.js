ba578836f43dee852fc41871dba932d82ef3bb42
flameastro <gabrielgoulartbnu@gmail.com>
2026-09-15 19:23:01 -0300

    add explosion animation

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,6 @@ const pointsText = document.querySelector("#points")
 let points = 0
 const body = document.body
 
-
-
-// button theme
 const lightColor = "#e3e3e3"
 const darkColor = "#272727"
 let mode = "light"
@@ -23,7 +20,6 @@ function toggleTheme() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>
             </button>
         `
-
     } else {
         body.style.backgroundColor = darkColor
         body.style.color = lightColor
@@ -33,8 +29,6 @@ function toggleTheme() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>
             </button>
         `
-
-
     }
 
     mode = mode == "light" ? "dark" : "light"
@@ -49,6 +43,7 @@ function toggleTheme() {
 function updatePoints() {
     points += 1
     pointsText.innerText = points
+
     return points
 }
 
@@ -84,8 +79,8 @@ function createDiv() {
 function interact(div) {
     // Generate new random position
     div.addEventListener("click", () => {
-        randomPos(div)
-        updatePoints(points)
+        explosion(div)
+        updatePoints()
     })
 }
 
@@ -102,6 +97,65 @@ function changeColor() {
     getColor.addEventListener("change", () => {
         changeColor()
     })
+}
+
+
+// Efeito de explosão
+function explosion(div) {
+    // getting square position
+    const rect = div.getBoundingClientRect()
+
+    // getting center of square
+    const centerX = rect.left + rect.width / 2
+    const centerY = rect.top + rect.height / 2
+
+    // getting current color
+    const color = document.querySelector("#color").value
+
+    // creating particles
+    for (let i = 0; i < 20; i++) {
+        const particle = document.createElement("div")
+
+        // add particle class
+        particle.classList.add("particle")
+
+        // setting initial position
+        particle.style.left = `${centerX}px`
+        particle.style.top = `${centerY}px`
+
+        // random direction
+        const angle = Math.random() * Math.PI * 2
+        const distance = Math.random() * 100 + 50
+
+        // calculating X and Y
+        const x = Math.cos(angle) * distance
+        const y = Math.sin(angle) * distance
+
+        // setting movement
+        particle.style.setProperty("--x", `${x}px`)
+        particle.style.setProperty("--y", `${y}px`)
+
+        // setting particle color
+        particle.style.backgroundColor = color
+        particle.style.boxShadow = `0 0 8px ${color}`
+
+        // append particle to body
+        body.appendChild(particle)
+
+        // remove particle after animation
+        setTimeout(() => {
+            particle.remove()
+        }, 600)
+    }
+
+    // add explosion animation to square
+    div.classList.add("exploding")
+
+    // remove animation and generate new position
+    setTimeout(() => {
+        div.classList.remove("exploding")
+        randomPos(div)
+    }, 250)
 }
 
 

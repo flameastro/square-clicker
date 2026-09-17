67edd8c5a948ae41570a648630ac7561849b24ae
flameastro <gabrielgoulartbnu@gmail.com>
2026-09-16 21:58:12 -0300

    refactor: translate into English variable names

--- scripts/script.js
+++ scripts/script.js
@@ -64,34 +64,22 @@ function applyTheme() {
 
         themeDiv.innerHTML = `
             <button class="theme">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <circle cx="12" cy="12" r="4"/>
-                    <path d="M12 2v2"/>
-                    <path d="M12 20v2"/>
-                    <path d="m4.93 4.93 1.41 1.41"/>
-                    <path d="m17.66 17.66 1.41 1.41"/>
-                    <path d="M2 12h2"/>
-                    <path d="M20 12h2"/>
-                    <path d="m6.34 17.66-1.41 1.41"/>
-                    <path d="m19.07 4.93-1.41 1.41"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <circle cx="12" cy="12" r="4"/> <path d="M12 2v2"/> <path d="M12 20v2"/> <path d="m4.93 4.93 1.41 1.41"/> <path d="m17.66 17.66 1.41 1.41"/> <path d="M2 12h2"/> <path d="M20 12h2"/> <path d="m6.34 17.66-1.41 1.41"/> <path d="m19.07 4.93-1.41 1.41"/>
                 </svg>
             </button>
         `
     }
 
-    // Pega o botão novo depois do innerHTML
     document.querySelector(".theme").addEventListener("click", toggleTheme)
 }
 
 function toggleTheme() {
     mode = mode === "light" ? "dark" : "light"
-
     localStorage.setItem("mode", JSON.stringify(mode))
 
     applyTheme()
 }
 
-// Aplica o tema salvo ao carregar
 applyTheme()
 
 function updatePoints() {
